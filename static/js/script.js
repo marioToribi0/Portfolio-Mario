9ae799dbd1e1d5f710d7f7a858c9f2b0667ff234
Mario Toribio <yuniormtr@gmail.com>
2021-11-18 10:54:49 -0500

    loading

--- static/js/script.js
+++ static/js/script.js
@@ -16,35 +16,35 @@ setTimeout(function() { document.addEventListener("mousemove", parallax);}, 5000
 
 // Navbar
 // /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   let root = document.querySelector(':root');
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  let root = document.querySelector(':root');
 
-//     if ((prevScrollpos > currentScrollPos) && !(document.querySelector(".navbar_").style.position == "absolute")) {
-//         document.querySelector(".navbar_").style.position = "fixed";
-//     } else {
-//         // document.getElementById("navbar").style.top ="-100px";
-//         document.querySelector(".navbar_").style.position = "fixed";
-//     }
+    if ((prevScrollpos > currentScrollPos) && !(document.querySelector(".navbar_").style.position == "absolute")) {
+        document.querySelector(".navbar_").style.position = "fixed";
+    } else {
+        // document.getElementById("navbar").style.top ="-100px";
+        document.querySelector(".navbar_").style.position = "fixed";
+    }
 
-//     if(currentScrollPos!=0){
-//       document.getElementById("navbar").style.backgroundColor = "white";
-//       root.style.setProperty('--navbar-bg', "white");
-//       root.style.setProperty('--navbar-color', "black");
-//       root.style.setProperty('--blend-mode', 'none');
-//       document.querySelector(".navbar_").style.paddingTop = "10px";
-//       document.querySelector(".navbar_").style.paddingBottom = "10px";
-//     }
-//   else{
-//     document.getElementById("navbar").style.backgroundColor = "transparent";
-//     root.style.setProperty('--navbar-bg', "black");
-//     root.style.setProperty('--navbar-color', "white");
-//     root.style.setProperty('--blend-mode', 'screen');
-//     document.querySelector(".navbar_").style.position = "absolute";
-//     document.querySelector(".navbar_").style.paddingTop = "20px";
-//       document.querySelector(".navbar_").style.paddingBottom = "20px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
+    if(currentScrollPos!=0){
+      document.getElementById("navbar").style.backgroundColor = "white";
+      root.style.setProperty('--navbar-bg', "white");
+      root.style.setProperty('--navbar-color', "black");
+      root.style.setProperty('--blend-mode', 'none');
+      document.querySelector(".navbar_").style.paddingTop = "10px";
+      document.querySelector(".navbar_").style.paddingBottom = "10px";
+    }
+  else{
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+    root.style.setProperty('--navbar-bg', "black");
+    root.style.setProperty('--navbar-color', "white");
+    root.style.setProperty('--blend-mode', 'screen');
+    document.querySelector(".navbar_").style.position = "absolute";
+    document.querySelector(".navbar_").style.paddingTop = "20px";
+      document.querySelector(".navbar_").style.paddingBottom = "20px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
