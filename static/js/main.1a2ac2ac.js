cadaf23fb620ee6c9a3e1b55db6d0dd784983549
Mario Toribio <yuniormtr@gmail.com>
2024-02-29 22:59:17 -0400

    Remove tensorflow

--- static/js/main.1a2ac2ac.js
+++ static/js/main.1a2ac2ac.js
@@ -9592,7 +9592,6 @@
         const r = JSON.parse(
             '{"Qr":{"u2":"Mario Toribio","r4":"Data Scientist // Machine Learning Engineer","A":"+2 years of experience across deep learning, analytics, statistic and development. I usually work with Azure, Tensorflow, SQL, Power BI and Python. I like programming and drinking coffee."},"f3":[{"id":1,"institution":"Instituto Tecnológico de Santo Domingo","qualification":"Mechatronic Engineer"},\
             {"id":2,"institution":"FuseMachines","qualification":"Microdegree, Artificial Intelligence"},\
-            {"id":3,"institution":"Tensorflow","qualification":"Tensorflow Developer Certificate"},\
             {"id":4,"institution":"DeepLearning.AI","qualification":"Deep Learning Specialization - DeepLearning.AI Tensorflow Developer"},\
             {"id":6,"institution":"Stanford University","qualification":"Machine Learning Specialization"}\
             ],"UY":[{"id":"3","href":"https://www.linkedin.com/in/mario-t-3538aa205/","path_d":"M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"},\
